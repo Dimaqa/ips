9279562dff8f9ecdc31c3982a939c27ec50ce952
Dimaqa <best2010a@ya.ru>
2020-05-14 18:07:36 +0300

    add writelock

--- lect7.cpp
+++ lect7.cpp
@@ -14,13 +14,16 @@ double par(void)
 	double S = 0.0;
 	step = 1.0 / (double)num;
 	double t = omp_get_wtime();
+	omp_lock_t writelock;
+	omp_init_lock(&writelock);
 	#pragma omp parallel for private(x) reduction(+:S) num_threads(num_of_threads)
 	for (i = 0; i < num; i++)
 	{
 		x = (i + 0.5)*step;
 		S = S + 4.0 / (1.0 + x * x);
-		#pragma omp critical
+		omp_set_lock(&writelock);
 		inc++;
+		omp_unset_lock(&writelock);
 	}
 	t = omp_get_wtime() - t;
 	pi = step * S;
